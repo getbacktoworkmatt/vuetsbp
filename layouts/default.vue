56d10c72253901d794c525c8a37b882096a4995e
matthew <matthew.mcarthur91@gmail.com>
2018-03-10 22:32:33 -0400

    init

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>headerspace</header>
+    <nuxt/>
+    <footer>footerspace</footer>
+  </div>
+</template>
+
+<!-- <script lang="ts">
+
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script> -->
